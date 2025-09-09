381c432a2e7131a8d7a0bf32c446b2d199741813
Brian Patrick <bjpatric@us.ibm.com>
2025-09-09 08:53:51 -0500

    initial commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitepress';
+import { withSidebar } from 'vitepress-sidebar';
+
+// https://vitepress.dev/reference/site-config
+const vitePressOptions = {
+  // VitePress's options here...
+  title: 'Shidoka',
+  description: 'Shidoka Design System Documentation',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+
+    editLink: {
+      pattern: (page) => {
+        // Replace this with your own collection mapping
+        const cmsPath = page.relativePath.replace('.md', '');
+
+        return `/admin/index.html#/edit/${cmsPath}`;
+      },
+    },
+  },
+};
+
+const vitePressSidebarOptions = {
+  // VitePress Sidebar's options here...
+  documentRootPath: '/docs',
+  capitalizeEachWords: true,
+  hyphenToSpace: true,
+  collapsed: false,
+};
+
+export default defineConfig(withSidebar(vitePressOptions, vitePressSidebarOptions));
---
+++ docs/components/button.md
@@ -0,0 +1,10 @@
+# [Button](https://shidoka-applications.netlify.app/?path=/docs/components-button--docs)
+
+## Example
+
+<iframe
+  src="https://shidoka-applications.netlify.app/?path=/story/components-button--button&singleStory=true"
+  width="100%"
+  height="500"
+  frameborder="0"
+></iframe>
